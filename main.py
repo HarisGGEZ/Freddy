345b71e568db619e5bfd2689b938f9ca8281ff85
HarisGGEZ <haris.sauban@gmail.com>
2023-12-21 13:08:34 +0100

    la till lite ljud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import map
 import animatronic
 import fnafgamereal
+from playsound import playsound
 Jagad = False
 
 karta = map.kartaKlass()
@@ -18,18 +19,19 @@ while True:
         karta.printKarta()
     if val == "väska":
         fnafgamereal.inventory()
-    if val == "höger" or val ==  "vänster" or val ==  "tillbaka"  or val ==  "fram" or  val == "kök" or val ==  "ner":
+    if val == "höger" or val ==  "vänster" or val ==  "tillbaka"  or val ==  "fram" or  val == "kök" or val ==  "ner" or val == "stanna":
         karta.flytt(val)
-        if Jagad == True:
+        if karta.jagad("") == True:
             if karta.run() == 5:
+                playsound('/audio/freddy.mp3')
                 print("dead")
                 break
         fredrik.move()
     fredrik.printMove()
     if fredrik.returnLocation() == karta.returnPlats():
         karta.jagad("same")
-    if karta.jagad("") == True:
-        Jagad = True
+    #if karta.jagad("") == True:
+        #Jagad = True
     
     
   
